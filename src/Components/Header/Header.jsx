58a09a733127ea6c4770b34c08d5c393d3d89b3f
Bemnet <bemnetkibret4@gmail.com>
2024-11-24 14:22:50 +0300

    Header styling Done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,66 +3,93 @@ import { Link } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css'; // Import Font Awesome CSS
 import './Header.css';
 
+const Dropdown = ({ children, buttonLabel, icon }) => (
+  <div className="header__dropdown">
+    <div className="dropdown">
+      <button className="dropbtn">
+        {icon && <img src={icon} alt="icon" height="20" className='dropdown-icon' />} {buttonLabel}
+        <i className="fa fa-caret-down"></i>
+      </button>
+      <div className="dropdown-content">
+        {children}
+      </div>
+    </div>
+  </div>
+);
+
+const SecondaryHeaderSection = ({ children }) => (
+  <div className="secondary-header__section">
+    {children}
+  </div>
+);
+
 const Header = () => {
   return (
-    <header className="header">
-      <div className="header__logo">
-        <Link to="/">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" alt="Amazon" height="30" />
-        </Link>
-      </div>
-      <div className="header__location">
-        <i className="fas fa-map-marker-alt"></i> {/* Location pin icon */}
-        <div>
-          Deliver to <br />
-          <strong>Ethiopia</strong>
+    <>
+      <header className="header">
+        <div className="header__logo">
+          <Link to="/">
+            <img src="../public/Amazon-logo-white.svg.png" alt="Amazon" height="30" />
+          </Link>
         </div>
-      </div>
-      <div className="header__search-bar">
-        <select className="search-select">
-          <option value="all">All</option>
-        </select>
-        <input className="search-input" type="text" placeholder="Search Amazon" />
-        <button className="search-button">
-          <i className="fa-solid fa-magnifying-glass"></i> {/* Search icon */}
-        </button>
-      </div>
-      <div className="header__dropdown">
-        <div className="dropdown">
-          <button className="dropbtn">
-            <img src="https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg" alt="US Flag" height="20" /> EN 
-            <i className="fa fa-caret-down"></i>
-          </button>
-          <div className="dropdown-content">
-            <Link to="/">EN</Link>
-            <Link to="/">ES</Link>
-            <Link to="/">FR</Link>
+        <div className="header__location">
+          <i className="fas fa-map-marker-alt"></i> {/* Location pin icon */}
+          <div>
+            Deliver to <br />
+            <strong>Ethiopia</strong>
           </div>
         </div>
-      </div>
-      <div className="header__dropdown">
-        <div className="dropdown">
-          <button className="dropbtn">
-            Hello, sign in <br /> <strong>Account & Lists</strong>
-            <i className="fa fa-caret-down"></i>
+        <div className="header__search-bar">
+          <select className="search-select">
+            <option value="all">All</option>
+          </select>
+          <input className="search-input" type="text" placeholder="Search Amazon" />
+          <button className="search-button">
+            <i className="fa-solid fa-magnifying-glass"></i> {/* Search icon */}
           </button>
-          <div className="dropdown-content">
-            <Link to="/">Account</Link>
-            <Link to="/">Orders</Link>
-            <Link to="/">Wishlist</Link>
+        </div>
+        <Dropdown buttonLabel="EN" icon="https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg">
+          <Link to="/">EN</Link>
+          <Link to="/">ES</Link>
+          <Link to="/">FR</Link>
+        </Dropdown>
+        <Dropdown buttonLabel={<><span>Hello, sign in</span><br /><strong>Account & Lists</strong></>}>
+          <Link to="/">Account</Link>
+          <Link to="/">Orders</Link>
+          <Link to="/">Wishlist</Link>
+        </Dropdown>
+        <Dropdown buttonLabel={<><span>Returns</span><br /><strong>& Orders</strong></>}>
+          <Link to="/orders">Your Orders</Link>
+          <Link to="/returns">Returns & Refunds</Link>
+        </Dropdown>
+        <div className="header__cart">
+          <Link to="/cart" className="cart-link">
+            <i className="fa-solid fa-cart-shopping"></i>
+            <span className="cart-count">0</span> {/* Placeholder for cart count */}
+          </Link>
+        </div>
+      </header>
+      
+      <nav className="secondary-header">
+        <div className="secondary-header__container">
+          <div className="secondary-header__left">
+            <SecondaryHeaderSection>
+              <Link to="/">
+                <i className="fa fa-bars" aria-hidden="true"></i> All
+              </Link>
+            </SecondaryHeaderSection>
+            <SecondaryHeaderSection><Link to="/deals">Today's Deals</Link></SecondaryHeaderSection>
+            <SecondaryHeaderSection><Link to="/customer-service">Customer Service</Link></SecondaryHeaderSection>
+            <SecondaryHeaderSection><Link to="/registry">Registry</Link></SecondaryHeaderSection>
+            <SecondaryHeaderSection><Link to="/gift-cards">Gift Cards</Link></SecondaryHeaderSection>
+            <SecondaryHeaderSection><Link to="/sell">Sell</Link></SecondaryHeaderSection>
+          </div>
+          <div className="secondary-header__right">
+            <SecondaryHeaderSection><Link to="/black-friday-deals">Shop Black Friday deals</Link></SecondaryHeaderSection>
           </div>
         </div>
-      </div>
-      <div className="header__orders">
-        Returns <br /> <strong>& Orders</strong>
-      </div>
-      <div className="header__cart">
-        <Link to="/cart" className="cart-link">
-          <i className="fa-solid fa-cart-shopping"></i>
-          <span className="cart-count">0</span> {/* Placeholder for cart count */}
-        </Link>
-      </div>
-    </header>
+      </nav>
+    </>
   );
 };
 
---
+++ src/Components/Header/LowerHeader.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function LowerHeader() {
+  return (
+    <>
+
+    <div>
+        <div className='left-links'>
+            <a href="#">All</a>
+            <a href="#">Today's Deals</a>
+            <a href="#">Customer Service</a>
+            <a href="#">Registry</a>
+            <a href="#">Gift Cards</a>
+            <a href="#">Sell</a>
+        </div>
+        <div className='right-link'><a href="#"><strong>Shop Black Friday deals</strong></a></div>
+    </div>
+
+    </>
+  )
+}
+
+export default LowerHeader
